b96a2be5f5e67c01d40add57a2203d0234bf5d16
Jose Alfaro <jalfaro@perceptiongrp.com>
2018-11-26 05:33:37 -0300

    CRUD.blade.php,  modelo y controlador de matches

--- resources/views/matches/editMatch.blade.php
+++ resources/views/matches/editMatch.blade.php
@@ -3,7 +3,44 @@
 @section('main-content')
 <!--contenido de las secciones: login y registro home, amigos, faq, perfil-->
 <div class="contenedor container-fluid">
-    
+    <div class="row">
+        <h1>Editar Partido</h1>
+        <div class="cont-form">
+            <form action="">
+            <div class="col-sm-12 col-md-6">
+                    <div>
+                        <label for="">Titulo</label><br>
+                        <input type="text" name="" id="">
+                    </div>
+                    <div>
+                        <label for="">Lugar</label><br>
+                        <input type="text" name="" id="">
+                    </div>
+                    <div>
+                        <label for="">costo</label><br>
+                        <input type="number" name="" id="">
+                    </div>
+                    <div >
+                        <label for="">Total de jugadores</label><br>
+                        <input type="number" name="" id="">
+                    </div>
+                    <div>
+                        <label for="">Descripcion</label><br>
+                        <input type="text" name="" id="">
+                    </div>
+                    <div>
+                        <label for="">Fecha</label><br>
+                        <input type="date" name="" id="">
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <br>
+                    <input class="btn btn-primary" type="button" value="Guardar Partido">
+                </div> 
+            </form>
+        </div>
+    </div>
 </div>
 <!--fin del contenido-->
 @endsection
