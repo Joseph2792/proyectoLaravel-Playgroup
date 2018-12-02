42b7954b2875ade53e9a4ba42bd7eee0e16dc7d2
Jose Alfaro <jalfaro@perceptiongrp.com>
2018-12-02 04:19:19 -0300

    migraciones de las tablas

---
+++ database/migrations/2018_12_02_051156_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('nickname');
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('avatar',100)->default('avatar_default.jpg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('nickname');
+            $table->dropColumn('country');
+            $table->dropColumn('city');
+            $table->dropColumn('avatar');
+        });
+    }
+}
