86ea3cf2288231a8760d8c41390f8fd092942f39
Jose Alfaro <jalfaro@perceptiongrp.com>
2018-12-02 21:52:19 -0300

    relaciones entre modelos Game y Team

--- app/Game.php
+++ app/Game.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'date',
+        'place',
+        'price',
+        'description',
+        'number_of_player',
+        'likes',
+        'team1_id',
+        'team2_id',
+    ];
+
+    public function team1(){
+        return $this->hasOne (Team::class, 'id', 'team1_id');
+    }
+
+    public function team2(){
+        return $this->hasOne (Team::class, 'id', 'team2_id');
+    }
 }
--- app/Team.php
+++ app/Team.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    protected $fillable = [
+        'sise',
+    ];
+
+    public function users(){
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
 }
